60bb96d6f269adad7ae17dc24220b5a03f20454a
luana.marques1 <luana.marques@sunoresearch.com.br>
2023-05-03 14:52:54 -0300

    feat: add components

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,64 @@
-import React, { useEffect } from 'react'
-import RNComponentsLibraryModule, {
-  Counter,
-} from 'react-native-first-components-library'
+import React, { useEffect, useState } from 'react'
+import TextField from '../../src/components/TextField'
+import { Container } from '../../src/components/Container'
+import { Title, Description, MinorTitle, MinorDescription, Regular } from '../../src/components/Texts'
+import { ToasterProps } from '../../src/components/Toaster';
+import DropDown from '../../src/components/Dropdown';
 
 const App = () => {
-  useEffect(() => {
-    console.log(RNComponentsLibraryModule)
-  })
+   const [text, setText] = useState('')
+   const [feedBack, setFeedback] = useState<ToasterProps>();
+   //   setEmailFeedback(type: 'warning', message: 'E-mail inválido' });
+   const [isFocused, setIsOpen] = useState(false)
+   const [selectedItem, setSelectedItem] = useState('Dúvidas')
 
-  return <Counter label={'Do something!'} />
+   return (
+      <Container>
+         <Title fontFamily='Montserrat'>Title</Title>
+         <MinorTitle fontFamily='Montserrat'>MinorTitle</MinorTitle>
+         <Description fontFamily='Montserrat' >Description</Description>
+         <MinorDescription fontFamily='Montserrat'>MinorDescription</MinorDescription>
+         <Regular fontFamily='Montserrat'>Regular</Regular>
+
+         <TextField
+            label="Seu email"
+            onChangeText={setText}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            feedback={feedBack}
+            accessibilityLabel="Digite seu email. Caixa de edição"
+            bigger
+         />
+         <DropDown
+              changeIsOpen={setIsOpen}
+              selected={selectedItem}
+              hasLabel
+              icon="chevron-down"
+              options={[
+                {
+                  title: 'Dúvidas',
+                  onPress: () => setSelectedItem('Dúvidas'),
+                },
+                {
+                  title: 'Elogios',
+                  onPress: () => setSelectedItem('Elogios'),
+                },
+                {
+                  title: 'Renovação automática',
+                  onPress: () => setSelectedItem('Renovação automática'),
+                },
+                {
+                  title: 'Cancelamento de Assinatura',
+                  onPress: () => setSelectedItem('Cancelamento de Assinatura'),
+                },
+                {
+                  title: 'Dúvidas sobre ativos',
+                  onPress: () => setSelectedItem('Dúvidas sobre ativos'),
+                },
+              ]}
+            />
+      </Container>
+   )
 }
 
 export default App
